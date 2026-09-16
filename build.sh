3d7b21c16091956b045ad70a3c6a95bf04f00e4d
Daniel Hernández <danisnowman@gmail.com>
2026-09-15 19:25:23 -0600

    Add app icon

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ PREFIX=$(brew --prefix libmtp)
 APP="KindleMTP.app"
 
 rm -rf "$APP"
-mkdir -p "$APP/Contents/MacOS"
+mkdir -p "$APP/Contents/MacOS" "$APP/Contents/Resources"
+cp AppIcon.icns "$APP/Contents/Resources/AppIcon.icns"
 
 cat > "$APP/Contents/Info.plist" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
@@ -19,6 +20,7 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleExecutable</key><string>KindleMTP</string>
   <key>CFBundleIdentifier</key><string>local.kindlemtp</string>
   <key>CFBundlePackageType</key><string>APPL</string>
+  <key>CFBundleIconFile</key><string>AppIcon</string>
   <key>CFBundleShortVersionString</key><string>1.0</string>
   <key>CFBundleVersion</key><string>1</string>
   <key>LSMinimumSystemVersion</key><string>13.0</string>
